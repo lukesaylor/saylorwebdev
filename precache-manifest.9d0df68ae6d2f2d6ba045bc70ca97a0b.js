b0078ddf9fa27ad6389967da8ff6231a2dd033d3
Lukas Saylor <slappyhoursound@gmail.com>
2020-03-03 13:01:05 -0500

    Updates

---
+++ precache-manifest.9d0df68ae6d2f2d6ba045bc70ca97a0b.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ef534dd21ff8a96f7cb8e5d8d58e27e",
+    "url": "/portfoliosite/index.html"
+  },
+  {
+    "revision": "3d37bf59044d7e4f7550",
+    "url": "/portfoliosite/static/css/main.a414553a.chunk.css"
+  },
+  {
+    "revision": "c1f038a18613dd43b829",
+    "url": "/portfoliosite/static/js/2.4245c217.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/portfoliosite/static/js/2.4245c217.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d37bf59044d7e4f7550",
+    "url": "/portfoliosite/static/js/main.08fd98db.chunk.js"
+  },
+  {
+    "revision": "45f4ece765ebde11e008",
+    "url": "/portfoliosite/static/js/runtime-main.4bb42195.js"
+  },
+  {
+    "revision": "396571a5948cb569dd6ceec2f40d4d4e",
+    "url": "/portfoliosite/static/media/about.396571a5.png"
+  },
+  {
+    "revision": "35c0822f817f640a556261e34b938298",
+    "url": "/portfoliosite/static/media/aboutheader.35c0822f.png"
+  },
+  {
+    "revision": "e5c685e3f08110f0c3db6f312714c1b2",
+    "url": "/portfoliosite/static/media/bg.e5c685e3.png"
+  },
+  {
+    "revision": "c4ce29f3d8d818e21c893bde4d815089",
+    "url": "/portfoliosite/static/media/bootstrap-plain.c4ce29f3.svg"
+  },
+  {
+    "revision": "4f0a895306c5582f69987fbe063d2581",
+    "url": "/portfoliosite/static/media/contact.4f0a8953.png"
+  },
+  {
+    "revision": "840cba7ff2da0c68b8c4009df10ebe90",
+    "url": "/portfoliosite/static/media/contactheader.840cba7f.png"
+  },
+  {
+    "revision": "c02c2a9a5f0efbd5af57c8e03156cccf",
+    "url": "/portfoliosite/static/media/css3-plain.c02c2a9a.svg"
+  },
+  {
+    "revision": "2fd6cc98dc7826f02312a87ba864f6e4",
+    "url": "/portfoliosite/static/media/html5-plain.2fd6cc98.svg"
+  },
+  {
+    "revision": "175cfa63af9e184f41b33b0f2c8cf807",
+    "url": "/portfoliosite/static/media/illustrator-plain.175cfa63.svg"
+  },
+  {
+    "revision": "0154328fc3e7bfcdb3d9ca42a6f848b1",
+    "url": "/portfoliosite/static/media/javascript-plain.0154328f.svg"
+  },
+  {
+    "revision": "164d2e540e15efaea1fdf15e2b1688c8",
+    "url": "/portfoliosite/static/media/logo.164d2e54.png"
+  },
+  {
+    "revision": "8f85d2657215e5b03c70695d15f0f327",
+    "url": "/portfoliosite/static/media/nodejs-plain.8f85d265.svg"
+  },
+  {
+    "revision": "a799052fd1df6450741b2658ebfeac85",
+    "url": "/portfoliosite/static/media/photoshop-plain.a799052f.svg"
+  },
+  {
+    "revision": "76e2126f922cf945f78b5048e1f2659f",
+    "url": "/portfoliosite/static/media/portfolio.76e2126f.png"
+  },
+  {
+    "revision": "90a39155f675fc4bfdb3d8000c749224",
+    "url": "/portfoliosite/static/media/portfolioheader.90a39155.png"
+  },
+  {
+    "revision": "e9f3d01c242e086f895bde131803b451",
+    "url": "/portfoliosite/static/media/react-original.e9f3d01c.svg"
+  },
+  {
+    "revision": "e2a72d50ef49bdd292dd1ecd0528fc63",
+    "url": "/portfoliosite/static/media/skills.e2a72d50.png"
+  },
+  {
+    "revision": "770ee0856e3b9d1d2eecfd393f196570",
+    "url": "/portfoliosite/static/media/skillsheader.770ee085.png"
+  }
+]);
